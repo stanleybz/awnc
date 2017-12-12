17812bfc06f4f3bba70c62289f3a0da2947381be
staleybz <>
2017-12-12 16:32:58 +0800

    renew

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import requests
 import redis
+from firebase import firebase
 from bs4 import BeautifulSoup
+from time import sleep
+
 
 class Main:
-
-    redisHost = 
-    redisPort = 
-    redisPassword = 
 
     def __init__(self):
         # self.getArticleUrl()
         self.connectRedis()
-
-    def connectRedis(self):
-        self.r = redis.StrictRedis(\
-            host = self.redisHost,\
-            port = self.redisPort,\
-            db = 0,\
-            password = self.redisPassword\
-        )
+        self.firebase = firebase.FirebaseApplication('https://firebaseio.com/', authentication=None)
 
     def getAllRow(self):
         for key in self.r.scan_iter("*"):
@@ -30,7 +22,7 @@ class Main:
         cats = ['home', 'news', 'blog', 'animationworld', 'vfxworld']
         for i in range(len(cats)):
             theCat = cats[i]
-            for x in range(0, 5):
+            for x in range(0, 1):
                 url = 'https://www.awn.com/'+theCat+'?page='+str(x)
                 print('downloading: ' + url)
                 response = requests.get(url)
@@ -39,9 +31,70 @@ class Main:
                 for theArticle in articles:
                     readMore = theArticle.find('div', class_='read-more')
                     theLink = readMore.find('a', href=True)
-                    self.r.set(theLink['href'], 0)
+                    if self.r.get(theLink['href']) is None:
+                        # Save if not exists
+                        self.r.set(theLink['href'], 0)
 
+    def crawContent(self, url):
+        # print(link)
+        response = requests.get('https://awn.com')
+        print(response)
+        return
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        #
+        # Get title
+        #
+        title = soup.find('h1', {'id': 'page-title'}).getText().lstrip()
+        # print(title)
+
+        article = soup.find('article')
+
+        #
+        # Get abstract
+        #
+        abstract = article.find('div', {'class': 'field-name-field-abstract'})\
+                        .find('div', {'class': 'field-items'})\
+                        .find('div', {'class': 'field-item'})\
+                        .find('p').getText().lstrip()
+        # print(abstract)
+
+        #
+        # Get submitted
+        #
+        submitted = article.find('footer', {'class': 'submitted'})
+        # print(submitted)
+
+        #
+        # Get body
+        #
+        body = article.find('div', {'class': 'content'})\
+                        .find('div', {'class': 'field-name-body'})
+        # print(body)
+
+        #
+        # Get tags
+        #
+        tags = article.find('div', {'class': 'content'})\
+                        .find('div', {'class': 'field-name-field-tags'})\
+                        .find('div', {'class': 'field-items'})\
+                        .find_all('div', {'class': 'field-item'})
+                        # .find('a')
+        allTags = []
+        for tag in tags:
+            theTag = tag.find('a').getText().lstrip()
+            allTags.append(theTag)
+        # print(allTags)
+        #
+        # result = self.firebase.post('/' + url, {\
+        #             'title':title,\
+        #             'abstract':abstract,\
+        #             'submitted':submitted,\
+        #             'body':body,\
+        #             'allTags':allTags,\
+        #         })
 
 main = Main()
 # main.getArticleUrl()
-main.getAllRow()
+# main.getAllRow()
+main.crawContent('https://www.awn.com/news/neill-blomkamp-uses-unity-2017-unleash-short-adam-mirror')
